d05d262dca89016e6f53f9a8ece87cb2c3e080f4
julian-salinas <juliansalinasm@gmail.com>
2021-09-17 04:31:45 -0300

    added pause, resume and stop

--- src/app.py
+++ src/app.py
@@ -12,33 +12,27 @@ bot = commands.Bot(command_prefix = '>')
 async def ping(ctx): 
     await ctx.send("pong!") 
 
-@bot.command(name = 'play')
-async def play(ctx):
+
+@bot.command(name = 'play') # reproducir musica con el comando >play <song>
+async def play(ctx, url:str): # Conectar el bot y reproducir una cancion
     author_channel = ctx.message.author.voice.channel
     
-    if not author_channel:
+    if not author_channel: # Verificar que la persona que envio el comando este en un canal de voz
         await ctx.send("Tenés que conectarte a un canal de voz para poner musica wachin")
         return
 
-    voice = get(bot.voice_clients, guild = ctx.guild)
+    voice = get(bot.voice_clients, guild = ctx.guild) # comando que nos permite usar "la voz"
 
-    if voice and voice.is_connected():
+    if voice and voice.is_connected(): # ir al canal de voz del autor del mensaje que prendio el bot
         await voice.move_to(author_channel)
     else:
         voice = await author_channel.connect()
 
+    # Aca termina de conectarse el bot y empieza con el proceso de reproducir musica
 
-@bot.command(name = 'disc')
-async def disc(ctx):
-    author_channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild = ctx.guild)
-    await voice.disconnect()
-
-
-@bot.command(name = 'repr')
-async def repr(ctx, url:str):
     active_song = os.path.isfile('song.mp3')
-    try:
+
+    try: # ir borrando las canciones a medida que se reproducen para limpiar espacio
         if active_song:
             os.remove("song.mp3")
             print(f"song {active_song} removed")
@@ -50,9 +44,7 @@ async def repr(ctx, url:str):
 
     await ctx.send("Anda todo fresco :cold_face:")
 
-    voice = get(bot.voice_clients, guild = ctx.guild)
-
-    youtube_sound = {
+    youtube_sound = { # configuraciones de descarga del sonido
         'format' : 'bestaudio/best',
         'postprocessor' : [{
             'key'              : 'FFmnegExtractAudio',
@@ -61,11 +53,11 @@ async def repr(ctx, url:str):
         }] 
     }
 
-    with youtube_dl.YoutubeDL(youtube_sound) as ydl:
+    with youtube_dl.YoutubeDL(youtube_sound) as ydl: # descargar la cancion del link
         print("Downloading song...")
         ydl.download([url])
 
-    for file in os.listdir("./"):
+    for file in os.listdir("./"): # renombrar la cancion a song.mp3
         if file.endswith(".mp3") or file.endswith(".m4a"):
             name = file
             print(f"Renaming file: {file}")
@@ -78,6 +70,55 @@ async def repr(ctx, url:str):
     song_name = name.rsplit("-", 2)
 
     await ctx.send(f"Ahora suena: {song_name[0]}") 
+
+
+@bot.command(name = 'pause')
+async def pause(ctx):
+    voice = get(bot.voice_clients, guild = ctx.guild) # comando que nos permite usar "la voz"
+
+    if voice and voice.is_playing():
+        print("Pause")
+        voice.pause()
+        await ctx.send("Musica pausada")
+    
+    else:
+        print("Error pausing, nothing to pause")
+        await ctx.send("No hay nada reproduciendose")
+
+
+@bot.command(name = 'resume')
+async def pause(ctx):
+    voice = get(bot.voice_clients, guild = ctx.guild) # comando que nos permite usar "la voz"
+
+    if voice and voice.is_paused():
+        print("Resuming song")
+        voice.resume()
+        await ctx.send("Volvimo")
+    
+    else:
+        print("Error resuming, nothing playing")
+        await ctx.send("No hay nada pausado")
+
+
+@bot.command(name = 'stop')
+async def stop(ctx):
+    voice = get(bot.voice_clients, guild = ctx.guild) # comando que nos permite usar "la voz"
+
+    if voice and voice.is_playing():
+        print("Stopping music")
+        voice.stop()
+        await ctx.send("Se detuvo la reproduccion")
+    
+    else:
+        print("Error stopping, nothing playing")
+        await ctx.send("No hay nada reproduciéndose")
+
+
+@bot.command(name = 'disc') # desconectar el bot del canal de voz
+async def disc(ctx):
+    author_channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild = ctx.guild)
+    await voice.disconnect()
 
 
 if __name__ == '__main__':
