9413dce39972a27597a2d6ba52e1e02bff92a465
julian-salinas <jusalinas@frba.utn.edu.ar>
2021-09-18 15:53:16 -0300

    hide token

--- src/bot.py
+++ src/bot.py
@@ -1,15 +1,13 @@
+import os
 import discord
 from discord.ext import commands
 
 from music_commands import MusicCog
 
-TOKEN = "ODg4MzA1NzA5NjM5NDA1NTc5.YUQxKQ.5f-wv--V3Phzclvuo0k8Z2e8sDQ"
-
 Bot = commands.Bot(command_prefix = '/')
 
 Bot.add_cog(MusicCog(Bot))
 
-
 if __name__ == '__main__':
     print("Bot corriendoououo!")
-    Bot.run(TOKEN)
+    Bot.run(os.getenv("TOKEN"))
